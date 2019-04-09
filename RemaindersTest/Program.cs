5d09371227ebc63dd71659ce32081780ba0930d4
Andrew Edson <docedson@gmail.com>
2019-04-09 13:06:14 -0500

    Add project files.

---
+++ RemaindersTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemaindersTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine(quotient);
+            Console.WriteLine(remainder);
+
+            Console.WriteLine(b * quotient + remainder);
+        }
+    }
+}
